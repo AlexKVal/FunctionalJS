295f66a35b122b3196fe79f55f876eeed00d42a5
AlexKVal <alexkval@gmail.com>
2015-03-30 11:42:49 +0300

    18_function_call

--- 18_function_call.js
+++ 18_function_call.js
@@ -1,11 +1,1 @@
-module.exports = Array.prototype.slice.call.bind(Array.prototype.slice)
-
-// var slice = Array.prototype.slice.call.bind(Array.prototype.slice)
-//
-// function probe() {
-//   console.dir(arguments);
-//   var args = slice(arguments)
-//   console.dir(args);
-// }
-//
-// probe('f', 'b', 'c')
+module.exports = Function.prototype.call.bind(Array.prototype.slice)
